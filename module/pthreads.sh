94d8cfb3934c1ac2f09947cbcafd5aeed1a0fca4
wr4ng1er <8627415@qq.com>
2015-04-18 14:40:52 +0800

    first commit

---
+++ module/pthreads.sh
@@ -0,0 +1,11 @@
+
+
+git clone https://github.com/krakjoe/pthreads.git
+cd pthreads
+phpize
+./configure --with-php-config=/opt/php5/bin/php-config 
+make ; make install
+
+
+
+
---
+++ module/swoole.sh
@@ -0,0 +1,9 @@
+
+
+git clone https://github.com/swoole/swoole-src.git
+cd swoole-src
+phpize
+./configure
+make && make install
+
+
